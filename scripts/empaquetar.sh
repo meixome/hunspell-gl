6a1479f79c0dbce0c6be23d936c1a92afbc59aa9
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2013-10-11 00:34:57 +0200

    Engadíronse «preinscribir» e «redirixir» ao módulo de comunidade, e preparouse o guión de empaquetación para crear un paquete de comunidade, e usar o código gl_ES.

--- scripts/empaquetar.sh
+++ scripts/empaquetar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rootFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+code="gl_ES"
 
 pushd $rootFolder &> /dev/null
 
@@ -8,12 +9,27 @@ pushd $rootFolder &> /dev/null
 rm -rf ./build
 
 # Construír e empaquetar o corrector principal (VOLG).
-scons aff=norma dic=volga rep=comunidade,galipedia
+scons aff=norma dic=volga rep=comunidade,galipedia code=${code}
 pushd build &> /dev/null
 packageName="hunspell-gl-$(date -u +"%Y%m%d")"
 mkdir ${packageName}
-mv gl.aff ${packageName}/gl.aff
-mv gl.dic ${packageName}/gl.dic
+mv ${code}.aff ${packageName}/${code}.aff
+mv ${code}.dic ${packageName}/${code}.dic
+for filename in $(find .. -name "*.txt")
+do
+  cp "$filename" "${packageName}/"
+done
+tar -cavf ${packageName}.tar.xz ${packageName} &> /dev/null
+mv ${packageName}.tar.xz ../${packageName}.tar.xz
+popd &> /dev/null
+
+# Construír e empaquetar o corrector da comunidade.
+scons aff=norma,trasno,unidades dic=comunidade,galipedia,iso639,iso4217,trasno,unidades,volga rep=comunidade,galipedia code=${code}
+pushd build &> /dev/null
+packageName="hunspell-gl-comunidade-$(date -u +"%Y%m%d")"
+mkdir ${packageName}
+mv ${code}.aff ${packageName}/${code}.aff
+mv ${code}.dic ${packageName}/${code}.dic
 for filename in $(find .. -name "*.txt")
 do
   cp "$filename" "${packageName}/"
