e9c70f819713e74f4b86569aacf3bcbe1709dcb8
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2014-05-14 21:41:39 +0200

    Engadiuse «galipedia/onomástica/toponimia/localidades/mozambique».

--- scripts/xerador/galipedia.py
+++ scripts/xerador/galipedia.py
@@ -626,6 +626,7 @@ def loadGeneratorList():
     generators.append(GalipediaLocalidadesGenerator(u"Malaisia"))
     generators.append(GalipediaLocalidadesGenerator(u"Malí"))
     generators.append(GalipediaLocalidadesGenerator(u"México", [u"Cidades de {name}", u"Cidades prehispánicas de {name}", u"Concellos de {name}"]))
+    generators.append(GalipediaLocalidadesGenerator(u"Mozambique", parsingMode="FirstSentence"))
     generators.append(GalipediaLocalidadesGenerator(u"Nepal", [u"Cidades do {name}"]))
     generators.append(GalipediaLocalidadesGenerator(u"Oceanía"))
     generators.append(GalipediaLocalidadesGenerator(u"Países Baixos", [u"Cidades dos {name}"]))
