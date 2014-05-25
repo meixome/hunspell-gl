0af35501ab3c9bda67d144fcfbae283efa22d8e7
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2014-05-25 09:31:48 +0200

    Engadiuse «udc/siglas».

--- scripts/xerar/udc.py
+++ scripts/xerar/udc.py
@@ -11,7 +11,7 @@ import generator
 
 contentCache = ContentCache("udc")
 abbreviationsPdfUrl = u"http://download.microsoft.com/download/A/0/B/A0B1A66A-5EBF-4CF3-9453-4B13BB027F1F/Phd_thesis_DB_v21.pdf"
-administrativeAbbreviationsPdfUrl = u"http://www.udc.es/snl/documentospdf/Libro_Criterios_lingua.pdf"
+languageUsageCriteriaPdfUrl = u"http://www.udc.es/snl/documentospdf/Libro_Criterios_lingua.pdf"
 
 
 class AbbreviationsGenerator(generator.Generator):
@@ -74,7 +74,7 @@ class AdministrativeAbbreviationsGenerator(generator.Generator):
 
     def generateFileContent(self):
 
-        filePath = contentCache.downloadFileIfNeededAndGetLocalPath(administrativeAbbreviationsPdfUrl)
+        filePath = contentCache.downloadFileIfNeededAndGetLocalPath(languageUsageCriteriaPdfUrl)
         pdfParser = PdfParser(filePath)
 
         entries = {}
@@ -120,16 +120,126 @@ class AdministrativeAbbreviationsGenerator(generator.Generator):
                 entries[line.strip()] = comment
                 comment = None
 
-        dictionary  = u"# Relación de abreviaturas máis frecuentes\n"
-        dictionary += u"# {}\n".format(administrativeAbbreviationsPdfUrl)
+        dictionary  = u"# Relación de abreviaturas máis frecuentes na linguaxe administrativa\n"
+        dictionary += u"# {}\n".format(languageUsageCriteriaPdfUrl)
         dictionary += u"\n"
         for entry in formatEntriesAndCommentsForDictionary(entries, u"abreviatura"):
             dictionary += entry
         return dictionary
 
 
+class AcronymsGenerator(generator.Generator):
+
+    def __init__(self):
+        self.resource = u"udc/siglas/xeral.dic"
+
+
+    def generateFileContent(self):
+
+        filePath = contentCache.downloadFileIfNeededAndGetLocalPath(languageUsageCriteriaPdfUrl)
+        pdfParser = PdfParser(filePath)
+
+        entries = {}
+        parsingStage = 0
+        entry = None
+        twoLineEntries = [u"CGENDL", u"CIXTEC", u"CORDIS", u"ECTS", u"EFQM", u"FAO", u"ISBN", u"ISO", u"ISSN", u"Unesco", "Unicef"]
+
+        for line in pdfParser.lines():
+
+            line = line.strip()
+
+            if parsingStage == 0:
+                if line == u"ANEXO IV. RELACIÓN DE SIGLAS E ACRÓNIMOS MÁIS HABITUAIS":
+                    parsingStage += 1
+                continue
+
+            elif parsingStage == 1:
+                if line == u"ANEXO V. RELACIÓN DE SIGLAS E ACRÓNIMOS MÁIS HABITUAIS DA UDC":
+                    break
+
+            if line in [u"CRITERIOS PARA O USO DA LINGUA", u"ANEXO IV. RELACIÓN DE SIGLAS E ACRÓNIMOS MÁIS HABITUAIS"]:
+                continue
+            if line.isdigit():
+                continue
+
+            if not entry:
+                entry = line.strip()
+            elif entry in twoLineEntries: # Caso especial que hai que xestionar como malamente se poida.
+                if entry in entries:
+                    entries[entry] += u" " + line.strip()
+                    entry = None
+                else:
+                    entries[entry] = line.strip()
+            else:
+                entries[entry] = line.strip()
+                entry = None
+
+        dictionary  = u"# Relación de siglas e acrónimos máis frecuentes\n"
+        dictionary += u"# {}\n".format(languageUsageCriteriaPdfUrl)
+        dictionary += u"\n"
+        for entry in formatEntriesAndCommentsForDictionary(entries, u"sigla"):
+            dictionary += entry
+        return dictionary
+
+
+class UdcAcronymsGenerator(generator.Generator):
+
+    def __init__(self):
+        self.resource = u"udc/siglas/udc.dic"
+
+
+    def generateFileContent(self):
+
+        filePath = contentCache.downloadFileIfNeededAndGetLocalPath(languageUsageCriteriaPdfUrl)
+        pdfParser = PdfParser(filePath)
+
+        entries = {}
+        parsingStage = 0
+        entry = None
+        twoLineEntries = [u"ASISTA",]
+
+        for line in pdfParser.lines():
+
+            line = line.strip()
+
+            if parsingStage == 0:
+                if line == u"ANEXO V. RELACIÓN DE SIGLAS E ACRÓNIMOS MÁIS HABITUAIS DA UDC":
+                    parsingStage += 1
+                continue
+
+            elif parsingStage == 1:
+                if line.startswith(u"ÚLTIMAS PUBLICACIÓNS"):
+                    break
+
+            if line in [u"CRITERIOS PARA O USO DA LINGUA", u"ANEXO V. RELACIÓN DE SIGLAS E ACRÓNIMOS MÁIS HABITUAIS DA UDC"]:
+                continue
+            if line.isdigit():
+                continue
+
+            if not entry:
+                entry = line.strip()
+            elif entry in twoLineEntries: # Caso especial que hai que xestionar como malamente se poida.
+                if entry in entries:
+                    entries[entry] += u" " + line.strip()
+                    entry = None
+                else:
+                    entries[entry] = line.strip()
+            else:
+                entries[entry] = line.strip()
+                entry = None
+
+        dictionary  = u"# Relación de siglas e acrónimos máis frecuentes na UDC\n"
+        dictionary += u"# {}\n".format(languageUsageCriteriaPdfUrl)
+        dictionary += u"\n"
+        for entry in formatEntriesAndCommentsForDictionary(entries, u"sigla"):
+            dictionary += entry
+        return dictionary
+
+
 def loadGeneratorList():
     generators = []
+    generators.append(AcronymsGenerator())
     generators.append(AbbreviationsGenerator())
     generators.append(AdministrativeAbbreviationsGenerator())
+    generators.append(UdcAcronymsGenerator())
     return generators
